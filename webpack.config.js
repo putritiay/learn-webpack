9658e32f7b2347231c8e9ecbef24f5b98f53f30c
putritiay <putritiay@gmail.com>
2022-10-04 03:08:39 +0700

    menerapkan teknik asset management menggunakan  Webpack Dev Server, HTML Loader, File Loader untuk mengelola aset gambar pada app

--- webpack.config.js
+++ webpack.config.js
@@ -33,6 +33,14 @@ module.exports = {
           "sass-loader",
         ],
       },
+      {
+        test: /\.html$/i,
+        loader: "html-loader",
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg)$/i,
+        type: "asset/resource",
+      },
     ],
   },
 };
--- webpack.dev.js
+++ webpack.dev.js
@@ -5,11 +5,19 @@ const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = merge(config, {
   mode: "development",
+  devServer: {
+    static: {
+      directory: path.join(__dirname, "src"),
+    },
+    compress: true,
+    port: 3000,
+    liveReload: true,
+  },
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "main.js",
+    assetModuleFilename: "img/[name][ext]",
     clean: true,
   },
   plugins: [new MiniCssExtractPlugin()],
-  watch: true,
 });
